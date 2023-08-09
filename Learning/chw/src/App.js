d237087e65cf9ff579e423522086e89e15a3068d
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-08-09 11:54:59 +0530

    .

--- Learning/chw/src/App.js
+++ Learning/chw/src/App.js
@@ -1,17 +1,27 @@
 // import Aboutus from "./components/Aboutus";
+// import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import React, { useState } from "react";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+  };
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
   };
   return (
@@ -22,6 +32,8 @@ function App() {
         mode={mode}
         toggleMode={toggleMode}
       />
+      {/* passing state variable alert  */}
+      {/* <Alert alert={alert} /> */}
       <div className="container">
         <Textform heading="Enter the text to analyze" mode={mode} />
       </div>
---
+++ Learning/chw/src/components/Alert.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function (props) {
+  const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  }
+  return (
+    <>
+      
+      <div
+        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong> {capitalize(props.alert.type)}</strong> : {props.alert.msg}
+        fields below.
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
+    </>
+  );
+}
