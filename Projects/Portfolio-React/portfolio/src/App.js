c809040212bf38c7baa41aac7b9f0e72d0b658e3
Ashutosh Pandey <pandeyashutosh603@gmail.com>
2023-10-12 00:11:58 +0530

    .

--- Projects/Portfolio-React/portfolio/src/App.js
+++ Projects/Portfolio-React/portfolio/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { MainBody } from './styles/Global.styled';
 
 function App() {
   return (
     <div className="App">
+
+    <MainBody/>
 
 
 
---
+++ Projects/Portfolio-React/portfolio/src/styles/Global.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainBody = styled.div`
+
+        background-color:#000;
+        height:50px;
+        width:100%;
+
+`
